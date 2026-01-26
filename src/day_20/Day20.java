0a448be2beb57e122efb2ac7243a3a4b634273a7
Mike Digdon <32070772+digdon@users.noreply.github.com>
2026-01-26 17:50:44 -0400

    Improved solution for day 20, plus a README.

--- src/day_20/Day20.java
+++ src/day_20/Day20.java
@@ -12,15 +12,28 @@ public class Day20 {
     private static void part1Sieve(int target) {
         long startTime = System.currentTimeMillis();
         
-        int limit = target / 10 / 2;
-        int[] houses = new int[limit + 1];
+//        int limit = target / 10 / 3;
+//        int[] houses = new int[limit + 1];
+//        
+//        for (int elf = 1; elf <= limit; elf++) {
+//            for (int i = elf; i <= limit; i += elf) {
+//                houses[i] += elf * 10;
+//            }
+//        }
+
+        int lowerBound = target / 10 / 5;
+        int upperBound = target / 10 / 3;
+        int[] houses = new int[upperBound - lowerBound + 1];
         
-        for (int elf = 1; elf <= limit; elf++) {
-            for (int i = elf; i <= limit; i += elf) {
-                houses[i] += elf * 10;
+        for (int elf = 1; elf <= upperBound; elf++) {
+            int remainder = lowerBound % elf;
+            int startHouse = lowerBound + (remainder == 0 ? 0 : elf - remainder);
+
+            for (int i = startHouse; i <= upperBound; i += elf) {
+                houses[i - lowerBound] += elf * 10;
             }
         }
-
+        
         int minHouse = 0;
         
         for (int i = 1; i < houses.length; i++) {
@@ -30,18 +43,33 @@ public class Day20 {
             }
         }
 
+        minHouse += lowerBound;
+        
         System.out.println(String.format("Part 1 (sieve): %d (%d ms)", minHouse, System.currentTimeMillis() - startTime));
     }
     
     private static void part2Sieve(int target) {
         long startTime = System.currentTimeMillis();
         
-        int limit = target / 11 / 2;
-        int[] houses = new int[limit + 1];
+//        int limit = target / 11 / 3;
+//        int[] houses = new int[limit + 1];
+//        
+//        for (int elf = 1; elf <= limit; elf++) {
+//            for (int i = elf, houseCount = 0; i <= limit && houseCount < 50; i += elf, houseCount++) {
+//                houses[i] += elf * 11;
+//            }
+//        }
         
-        for (int elf = 1; elf <= limit; elf++) {
-            for (int i = elf, houseCount = 0; i <= limit && houseCount < 50; i += elf, houseCount++) {
-                houses[i] += elf * 11;
+        int lowerBound = target / 10 / 5;
+        int upperBound = target / 10 / 3;
+        int[] houses = new int[upperBound - lowerBound + 1];
+        
+        for (int elf = 1; elf <= upperBound; elf++) {
+            int remainder = lowerBound % elf;
+            int startHouse = lowerBound + (remainder == 0 ? 0 : elf - remainder);
+
+            for (int i = startHouse, houseCount = startHouse / elf; i <= upperBound && houseCount < 50; i += elf, houseCount++) {
+                houses[i - lowerBound] += elf * 11;
             }
         }
 
@@ -53,6 +81,8 @@ public class Day20 {
                 break;
             }
         }
+        
+        minHouse += lowerBound;
 
         System.out.println(String.format("Part 2 (sieve): %d (%d ms)", minHouse, System.currentTimeMillis() - startTime));
     }
